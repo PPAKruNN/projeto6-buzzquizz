220a3d3c2d08bbd9b7be14bfd79b73ad73038112
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-04-20 01:27:46 -0300

    botões da tela principal trocam de tela; primeira sub página da tela de criação de quizz finalizada; código enxuto e alterado para receber melhor a renderização pelo JS

--- src/script/main.js
+++ src/script/main.js
@@ -11,6 +11,12 @@ function create_quizz() {
 
 }
 //------
+function play_quizz() {
+    //esconde a page 1 e mostra a page 2
+    document.getElementById('page_1').classList.add('hide');
+    document.getElementById('page_2').classList.remove('hide');
+}
+//------
 let ARR_3_1, button_3_1, FORbutton_3_1;
 //verifica se todos os inputs da page 3.1 estão preenchidos, se sim libera o botão
 function FUNCbutton_3_1() {
@@ -43,20 +49,33 @@ function FUNCbutton_3_1() {
 }
 function send_3_1() {
 
-    ARR_3_1 = [
-        {T_quizz: FORbutton_3_1[0].value},
-        {URL_quizz: FORbutton_3_1[1].value},
-        {QTD_P_quizz: FORbutton_3_1[2].value},
-        {QTD_N_quizz: FORbutton_3_1[2].value}
-    ];
+    ARR_3_1 = [{
+        T_quizz: FORbutton_3_1[0].value, 
+        URL_quizz: FORbutton_3_1[1].value,
+        QTD_P_quizz: FORbutton_3_1[2].value,
+        QTD_N_quizz: FORbutton_3_1[3].value
+    }];
+
     console.log(ARR_3_1);
-    console.log(parseInt(ARR_3_1[0].QTD_P_quizz));
 
-    let length = ARR_3_1[0].T_quizz.length; //será usada para o if verificar se a string tem entre 20 e 65 caracteres
+    const T = ARR_3_1[0].T_quizz.length; //será usada para o if verificar se a string tem entre 20 e 65 caracteres
+    const U = ARR_3_1[0].URL_quizz;
+    const P = parseInt(ARR_3_1[0].QTD_P_quizz); //será usada para o if verificar se a QTD de P é menor que 3
+    const N = parseInt(ARR_3_1[0].QTD_N_quizz); //será usada para o if verificar se a QTD de N é menor que 2
 
-    //apaga os value digitados nos inputs
-    FORbutton_3_1.forEach( array => {array.value = "";});
+    //função para verificar se o objeto é um url de imagem válido
+    function isURL(obj) {
+        var pattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\.(jpg|jpeg|png|gif)$/i;
+        return pattern.test(obj);
+    }
 
-    document.getElementById('page_3.1').classList.add('hide');
-    document.getElementById('page_3.2').classList.remove('hide');
+    if (T < 20 || T > 65 || isURL(U) === false || P < 3 || N < 2) {
+        alert('O título do Quizz deve ter entre 20 e 65 caracteres\nA URL deve ser válida\nA quantidade de perguntas deve ser maior que 2\nA quantidade de níveis deve ser maior que 1')
+    } else {
+        //apaga os value digitados nos inputs
+        FORbutton_3_1.forEach( array => {array.value = "";});
+
+        document.getElementById('page_3.1').classList.add('hide');
+        document.getElementById('page_3.2').classList.remove('hide');
+    }
 }
